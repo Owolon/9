0edc2653e5c75b7c4696f8b697594dd3e274427c
Owolon <keepoutmind@gmail.com>
2024-10-14 17:49:37 +0300

    Add files via upload

---
+++ Project9/Решение.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <math.h>
+#define CRT_NO_SECURE_DEPRECATE
+#define _USE_MATH_DEFINES
+#define ROW 9
+#define COL 9
+//1
+void main()
+{
+	int col, row;
+	printf("\n");
+	for (row = 1;row<=ROW;++row)
+	{
+		for (col = 1;col<=COL;++col)
+
+			printf("%5d", col*row);
+			printf("\n");
+
+			}
+	system("pause");
+
+	/*
+	int col, row;
+	COLa = 1;
+	printf("\n");
+	for (row = 1;row<=ROW;++row)
+	{
+		for (col = 1;col<=COLa;++col)
+			printf("%5d", col*row);
+			printf("\n");
+			}
+		COLa+=1
+	system("pause");
+	*/
+
+	int ROWl;
+	printf("\n");
+	for (int row = 40; row <= 80; row++)
+	{
+		ROWl = row;
+		for (int col = 1; col <= 10; col++)
+			printf("%4d\t", ROWl + 1);
+		}
+	printf("\n");
+}
